d673b59b12136363f9e52a937b5f8eab76251568
Andrey <i78901234567890@gmail.com>
2020-02-01 19:31:03 +0300

    prototype pattern

---
+++ prototype/prototype.py
@@ -0,0 +1,41 @@
+from abc import abstractmethod, ABC
+
+class Shepa(ABC):
+    X = ''
+    Y = ''
+    color = ''
+
+    def __init__(self, x, y, color):
+        self.X = x
+        self.Y = y
+        self.color = color
+
+    @abstractmethod
+    def copy(self):
+        pass
+
+
+class Circle(Shepa):
+
+    radius = 0
+
+    def __init__(self, x, y, color, radius):
+        super().__init__(x, y, color)
+        self.radius = radius
+
+    def copy(self):
+        copy_circle = Circle(self.X, self.Y, self.color, self.radius)
+        return copy_circle
+
+    def show_params(self):
+        print(self.X, self.Y, self.color, self.radius)
+
+
+small_circle = Circle(0, 0, 'green', 5)
+
+small_circle.show_params()
+
+small_circle_copy = small_circle.copy()
+
+small_circle_copy.show_params()
+
